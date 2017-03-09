312ccd6f91a833e0be3f6b23677c7c9bac9e5b96
pastiva6 <tonypastiva@hotmail.com>
2017-03-09 03:10:31 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,13 +9,18 @@ function respond() {
       botRegexAd=/^\/drunk/;botRegexGTA = /^\/klampz/; botRegexSC = /^\/sch/i; botODB = /(.*\s+)(.*odb)(\s+.*)/i; botDuck = /^\/duck/;
       botRegexP = /^\/ply/i;  botRegexTw = /^\/twitch/i; botRegexSb = /^\/millen/; botRegexSh = /^\/s/; botRegexWk = /^\/vance/; botRegexCC = /^\/jeff/;
       botRegexBye = /^\/stan/; botRegexRip =/^\/tony/; botRegexCan =/^\/willie/; botRegexTy=/^\/eagles/; botRegexBlog=/^\/immy/; 
-      botRegexL = /^\/teddy/; botRegexBtg = /^\/btg/; botRegexRtro = /^\/steve/;
+      botRegexL = /^\/teddy/; botRegexBtg = /^\/btg/; botRegexRtro = /^\/steve/; botRegexKab = /^\/kanax/;
   var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
                 ,"BAL","SD","DEN","MIN","ATL","KC","NYG","GB","DET","HOU","STL","CHI","CAR",
                 "MIA","BUF","SF","WAS","NYJ","TB"]
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
     postMessage("http://www.landaustore.co.uk/images/footwear/timberland/mens/timberland-mens-timberland-boots-mens-6-inch-boot-brown-37307.jpg");
+    this.res.end();
+  }
+      else if(request.text && botRegexKab.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://40.media.tumblr.com/8048deeee41ad603ab0c3fe5dbea7c0e/tumblr_nnjgpilUY81ut248go1_500.jpg")
     this.res.end();
   }
     else if(request.text && botRegexRtro.test(request.text)) {
