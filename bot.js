91ca44a4129ef10fc3e1dbdab4668daf73a3936c
pastiva6 <tonypastiva@hotmail.com>
2017-04-22 11:55:09 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,7 +24,7 @@ function respond() {
   }
             else if(request.text && botRegexNewz.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("http://i1380.photobucket.com/albums/ah170/Stephane_Baganizi/lockeroomvol7_zps08yghsml.jpg")
+    postMessage("http://i.imgur.com/yDh7TiV.jpg")
     this.res.end();
   } 
             else if(request.text && botRegexSclipz.test(request.text)) {
