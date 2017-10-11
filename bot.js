96182506a75e72a7da00526d67277973c4a7bf08
pastiva6 <tonypastiva@hotmail.com>
2017-10-11 01:22:52 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -160,7 +160,7 @@ function respond() {
   }
             else if(request.text && botRegexBanner.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("https://i.imgur.com/eLBKhzr.jpg")
+    postMessage("https://i.imgur.com/E1uGgGo.jpg")
     this.res.end();
   }
           else if(request.text && botRegexColts.test(request.text)) {
