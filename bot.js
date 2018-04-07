adda4ba72fad24ea51d713ddb5c06e15019c6206
pastiva6 <tonypastiva@hotmail.com>
2018-04-06 20:28:30 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,7 +25,7 @@ function respond() {
   }
             else if(request.text && botRegexNewz.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("https://i.imgur.com/TLqxVhy.jpg")
+    postMessage("https://i.imgur.com/qlnS4Va.jpg")
     this.res.end();
   } 
           else if(request.text && botRegexBmanz.test(request.text)) {
