625a5000a6c504f06ecfe10982b9370fcb1ebdde
pastiva6 <tonypastiva@hotmail.com>
2018-04-22 16:36:02 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -160,7 +160,7 @@ function respond() {
   }
             else if(request.text && botRegexBanner.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("https://i.imgur.com/L1j4siq.jpg")
+    postMessage("https://i.imgur.com/B8Pnuvy.jpg")
     this.res.end();
   }
           else if(request.text && botRegexColts.test(request.text)) {
